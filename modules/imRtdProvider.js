9e6ab7a7540d1442b44477c69253491f1b3a9f4f
masaki.shinke <tem.eknis@gmail.com>
2021-08-24 23:51:25 +0900

    update: vid

--- modules/imRtdProvider.js
+++ modules/imRtdProvider.js
@@ -19,14 +19,25 @@ import {
 } from '../src/utils.js'
 import {submodule} from '../src/hook.js';
 
-export const imuidLocalName = '__im_uid';
+export const imUidLocalName = '__im_uid';
+export const imVidCookieName = '_im_vid';
 export const imRtdLocalName = '__im_sids';
 export const storage = getStorageManager();
 const submoduleName = 'im';
-const storageMaxAge = 3600000; // 1 hour (30 * 60 * 1000)
-const storageMaxAgeForImuid = 1800000; // 30 minites (30 * 60 * 1000)
+const segmentsMaxAge = 3600000; // 1 hour (30 * 60 * 1000)
+const uidMaxAge = 1800000; // 30 minites (30 * 60 * 1000)
+const vidMaxAge = 97200000000; // 37 months ((365 * 3 + 30) * 24 * 60 * 60 * 1000)
 
-function getCustomBidderFunction (config, bidder) {
+function setImDataInCookie(value) {
+  storage.setCookie(
+    imVidCookieName,
+    value,
+    new Date(timestamp() + vidMaxAge).toUTCString(),
+    'none'
+  );
+}
+
+function getCustomBidderFunction(config, bidder) {
   const overwriteFn = deepAccess(config, `params.overwrites.${bidder}`)
 
   if (overwriteFn && isFn(overwriteFn)) {
@@ -37,60 +48,34 @@ function getCustomBidderFunction (config, bidder) {
 }
 
 /**
-* @param {string} bidderName
-* @param {object} data
-*/
-function getBidderFunction (bidderName) {
-  const biddersFunction = {
-    rubicon: function (bid, data) {
-      if (data.im_segments && data.im_segments.length) {
-        deepSetValue(bid, 'params.visitor.im_segments', data.im_segments)
-      }
-      return bid
-    },
-    openx: function (bid, data) {
-      if (data.im_segments && data.im_segments.length) {
-        deepSetValue(bid, 'params.visitor.im_segments', data.im_segments)
-      }
-      return bid
-    }
-  }
-  return biddersFunction[bidderName] || getDefaultFn();
-}
-
-function getDefaultFn() {
-  return function(bid, rtd) {
-    if (rtd.im_segments) {
-      deepSetValue(bid, 'params.customData.im_segments', rtd.im_segments);
-      const ortb2 = config.getConfig('ortb2') || {};
-      deepSetValue(ortb2, 'ext.data.im_segments', rtd.im_segments);
-      config.setConfig({ortb2: ortb2});
-    }
-  }
-}
-
-/**
  * Add real-time data & merge segments.
  * @param {Object} bidConfig
- * @param {Object} rtd
- * @param {Object} rtdConfig
+ * @param {Object} data
+ * @param {Object} moduleConfig
  */
-export function setRealTimeData(bidConfig, rtd, rtdConfig) {
+export function setRealTimeData(bidConfig, data, moduleConfig) {
   const adUnits = bidConfig.adUnits || getGlobal().adUnits;
   const utils = { deepSetValue, deepAccess, isFn, mergeDeep };
 
+  const ortb2 = config.getConfig('ortb2') || {};
+  deepSetValue(ortb2, 'user.data.ext.im_segments', data.im_segments);
+  config.setConfig({ortb2: ortb2});
+
+  if (moduleConfig.params.setGptKeyValues || !moduleConfig.params.hasOwnProperty('setGptKeyValues')) {
+    window.googletag = window.googletag || {
+      cmd: []
+    };
+    window.googletag.cmd.push(() => {
+      window.googletag.pubads().setTargeting('im_segments', data.im_segments);
+    });
+  }
+
   adUnits.forEach(adUnit => {
     adUnit.bids.forEach(bid => {
-      const { bidder } = bid;
-      const customFn = getCustomBidderFunction(rtdConfig, bidder);
-      const bidderFn = getBidderFunction(bidder);
-
-      if (customFn) {
-        customFn(bid, rtd, utils, bidderFn);
-      } else if (bidderFn) {
-        bidderFn(bid, rtd);
+      const overwriteFunction = getCustomBidderFunction(moduleConfig, bid.bidder);
+      if (overwriteFunction) {
+        overwriteFunction(bid, data, utils, moduleConfig);
       }
-      logInfo(bid);
       logInfo(utils.deepAccess(bid, 'params'));
     })
   });
@@ -100,48 +85,53 @@ export function setRealTimeData(bidConfig, rtd, rtdConfig) {
  * Real-time data retrieval from Intimate Merger
  * @param {Object} reqBidsConfigObj
  * @param {function} onDone
- * @param {Object} rtdConfig
- * @param {Object} userConsent
+ * @param {Object} moduleConfig
  */
-export function getRealTimeData(bidConfig, onDone, rtdConfig) {
-  logInfo(`rtdConfig:`);
-  logInfo(rtdConfig);
-  const cid = deepAccess(rtdConfig, 'params.cid');
+export function getRealTimeData(reqBidsConfigObj, onDone, moduleConfig) {
+  const cid = deepAccess(moduleConfig, 'params.cid');
   if (!cid) {
     logError('imRtdProvider requires a valid cid to be defined');
-    return undefined;
+    onDone();
+    return;
   }
   const sids = storage.getDataFromLocalStorage(imRtdLocalName);
   const mt = storage.getDataFromLocalStorage(`${imRtdLocalName}_mt`);
+  const localVid = storage.getCookie(imVidCookieName);
+  let apiUrl = `https://sync6.im-apps.net/${cid}/rtd`;
+  if (localVid) {
+    apiUrl += `&vid=${localVid}`;
+    setImDataInCookie(localVid);
+  }
+
   let expired = true;
-  if (Date.parse(mt) && Date.now() - (new Date(mt)).getTime() < storageMaxAge) {
+  if (Date.parse(mt) && Date.now() - (new Date(mt)).getTime() < segmentsMaxAge) {
     expired = false;
   }
 
   if (sids) {
-    logInfo('sids');
-    logInfo(sids);
-    setRealTimeData(bidConfig, {im_segments: sids}, rtdConfig);
+    logInfo(`sids: ${sids}`);
+    setRealTimeData(reqBidsConfigObj, {im_segments: sids}, moduleConfig);
     onDone();
     if (expired) {
-      getRealTimeDataAsync(bidConfig, rtdConfig, undefined);
+      getRealTimeDataAsync(reqBidsConfigObj, moduleConfig, apiUrl, undefined);
       return;
     }
+  }
+  if (!expired) {
     return;
   }
-  getRealTimeDataAsync(bidConfig, rtdConfig, onDone);
+  getRealTimeDataAsync(reqBidsConfigObj, moduleConfig, apiUrl, expired ? undefined : onDone);
 }
 
 /**
  * Async rtd retrieval from Intimate Merger
  * @param {Object} reqBidsConfigObj
- * @param {Object} rtdConfig
+ * @param {Object} moduleConfig
+ * @param {string} apiUrl
  * @param {function} onDone
  */
-export function getRealTimeDataAsync(bidConfig, rtdConfig, onDone) {
-  const cid = deepAccess(rtdConfig, 'params.cid');
-  const url = `https://sync6.im-apps.net/${cid}/rtd`;
-  ajax(url, {
+export function getRealTimeDataAsync(bidConfig, moduleConfig, apiUrl, onDone) {
+  ajax(apiUrl, {
     success: function (response, req) {
       let parsedResponse = {};
       if (req.status === 200) {
@@ -151,18 +141,22 @@ export function getRealTimeDataAsync(bidConfig, rtdConfig, onDone) {
           logError('unable to get Intimate Merger segment data');
         }
         if (parsedResponse.uid) {
-          const imuid = storage.getDataFromLocalStorage(imuidLocalName);
-          const imuidMt = storage.getDataFromLocalStorage(`${imuidLocalName}_mt`);
-          const imuidExpired = Date.parse(imuidMt) && Date.now() - (new Date(imuidMt)).getTime() < storageMaxAgeForImuid;
+          const imuid = storage.getDataFromLocalStorage(imUidLocalName);
+          const imuidMt = storage.getDataFromLocalStorage(`${imUidLocalName}_mt`);
+          const imuidExpired = Date.parse(imuidMt) && Date.now() - (new Date(imuidMt)).getTime() < uidMaxAge;
           if (!imuid || imuidExpired) {
-            storage.setDataInLocalStorage(imuidLocalName, parsedResponse.uid);
-            storage.setDataInLocalStorage(`${imuidLocalName}_mt`, new Date(timestamp()).toUTCString());
+            storage.setDataInLocalStorage(imUidLocalName, parsedResponse.uid);
+            storage.setDataInLocalStorage(`${imUidLocalName}_mt`, new Date(timestamp()).toUTCString());
           }
         }
+
+        if (parsedResponse.vid) {
+          setImDataInCookie(parsedResponse.vid);
+        }
+
         if (parsedResponse.segments) {
-          logInfo('parsedResponse.segments');
-          logInfo(parsedResponse.segments);
-          setRealTimeData(bidConfig, {im_segments: parsedResponse.segments}, rtdConfig);
+          logInfo(`parsedResponse.segments: ${parsedResponse.segments}`);
+          setRealTimeData(bidConfig, {im_segments: parsedResponse.segments}, moduleConfig);
           storage.setDataInLocalStorage(imRtdLocalName, parsedResponse.segments);
           storage.setDataInLocalStorage(`${imRtdLocalName}_mt`, new Date(timestamp()).toUTCString());
         }
--- modules/imRtdProvider.md
+++ modules/imRtdProvider.md
@@ -20,14 +20,8 @@ pbjs.setConfig(
                 name: "im",
                 waitForIt: true,
                 params: {
-                    cid: 5126 // Set your Intimate Merger Customer ID here for production
-                    overwrites: {
-                        rubicon: function (bid, data, utils, bidderFn) {
-                            if (bidderFn){
-                                bid = bidderFn(bid, data)
-                            }
-                        }
-                    }
+                    cid: 5126, // Set your Intimate Merger Customer ID here for production
+                    setGptKeyValues: true
                 }
             }
         ]
@@ -44,58 +38,4 @@ pbjs.setConfig(
 | waitForIt | Optional | Boolean | Required to ensure that the auction is delayed until prefetch is complete. Defaults to false but recommended to true | `true` |
 | params | Required | Object | Details of module params. | |
 | params.cid | Required | Number | This is the Customer ID value obtained via Intimate Merger. | `5126` |
-| params.overwrites | Object | See `Custom Bidder Setup` for details on how to define custom bidder functions.      | `{}` |
-
-### Custom Bidder Setup
-You can overwrite the default bidder function, for example to include a different set of segments or to support additional bidders. The below example modifies what first-party segments Magnite receives (segments from `gam` instead of `rubicon`). As best practise we recommend to first call `defaultFn` and then only overwrite specific key-values. The below example only overwrites `im` while `p_standard` are still set by `defaultFn` (if `rubicon` is an enabled `acBidder`).
-
-```javascript
-pbjs.setConfig({
-  ...,
-  realTimeData: {
-    auctionDelay: 50,
-    dataProviders: [{
-      name: 'im',
-      waitForIt: true,
-      params: {
-        acBidders: ['appnexus'],
-        maxSegs: 450,
-        overwrites: {
-          rubicon: function (bid, data, acEnabled, utils, defaultFn) {
-            if (defaultFn){
-              bid = defaultFn(bid, data, acEnabled)
-            }
-            if (data.gam && data.gam.length) {
-              utils.deepSetValue(bid, 'params.visitor.im_segments', data.gam)
-            }
-          }
-        }
-      }
-    }]
-  },
-  ...
-})
-```
-Any custom bidder function will receive the following parameters:
-
-| Name          | Type          | Description                             |
-| ------------- |-------------- | --------------------------------------- |
-| bid           | Object        | The bidder specific bidder object. You will mutate this object to set the appropriate targeting keys.       |
-| data          | Object        | An object containing Permutive segments |
-| data.{{bidder}} | string[]      | Segments exposed by SSP integration |
-| utils         | {}            | An object containing references to various util functions used by `permutiveRtdProvider.js`. Please make sure not to overwrite any of these. |
-| defaultFn     | Function      | The default function for this bidder. Please note that this can be `undefined` if there is no default function for this bidder (see `Supported Bidders`). The function expect the following parameters: `bid`, `data`, and will return `bid`. |
-
-### Testing
-
-To view an example of available segments returned:
-
-`gulp serve --modules=imRtdProvider`
-
-and then point your browser at:
-
-`http://localhost:9999/integrationExamples/gpt/imRtdProvider_example.html`
-
-
-
-
+| params.setGptKeyValues | Optional | Boolean | This is set targeting for GPT/GAM. Default setting is true. | `true` |
