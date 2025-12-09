8d675357f123ca9a215b93a42bd2c319b5d2db0f
eknis <tem.eknis@gmail.com>
2025-12-09 17:23:56 +0900

    IntimateMerger Analytics Adapter : refactoring

--- modules/imAnalyticsAdapter.js
+++ modules/imAnalyticsAdapter.js
@@ -4,15 +4,24 @@ import adapterManager from '../src/adapterManager.js';
 import { EVENTS } from '../src/constants.js';
 import { sendBeacon } from '../src/ajax.js';
 
-const DEBOUNCE_DELAY = 200; // 0.2 second
+const BID_WON_TIMEOUT = 800; // 0.8 second for initial batch
 const DEFAULT_CID = 5126;
 const API_BASE_URL = 'https://b6.im-apps.net/bids';
 
+// Send status flags
+const WON_SENT = 1;
+
+// Default values
+const EMPTY_CONSENT_DATA = {
+  gdprApplies: undefined,
+  gdpr: undefined,
+  usp: undefined,
+  gpp: undefined
+};
+
 const cache = {
   auctions: {},
-  ReqBidsData: null,
-  ReqBidsTimer: null,
-  wonBidsData: null,
+  wonBidsData: {},
   wonBidsTimer: null
 };
 
@@ -26,12 +35,13 @@ function getCid(options) {
 }
 
 /**
- * Build API endpoint URL
- * @param {string} cid - Customer ID
+ * Build API URL with CID from options
+ * @param {Object} options - Adapter options
  * @param {string} endpoint - Endpoint path
  * @returns {string} Full API URL
  */
-function buildApiUrl(cid, endpoint) {
+function buildApiUrlWithOptions(options, endpoint) {
+  const cid = getCid(options);
   return `${API_BASE_URL}/${cid}/${endpoint}`;
 }
 
@@ -59,29 +69,55 @@ function clearTimer(timer) {
 }
 
 /**
- * Initialize or reset page-level data cache
- * @param {string} pageUrl - Current page URL
- * @returns {Object} Initialized cache object
+ * Get consent data from bidder requests
+ * @param {Array} bidderRequests - Bidder requests array
+ * @returns {Object} Consent data object
  */
-function initializeReqBidsCache(pageUrl) {
+function getConsentData(bidderRequests) {
+  if (!bidderRequests || !bidderRequests[0]) {
+    return EMPTY_CONSENT_DATA;
+  }
+
+  const request = bidderRequests[0];
+  const gdprConsent = request.gdprConsent || {};
+  const uspConsent = request.uspConsent;
+  const gppConsent = request.gppConsent || {};
+
   return {
-    pageUrl,
-    timestamp: Date.now(),
-    auctions: []
+    gdprApplies: gdprConsent.gdprApplies,
+    gdpr: gdprConsent.consentString,
+    usp: uspConsent,
+    gpp: gppConsent.gppString
   };
 }
 
 /**
- * Initialize or reset won bids data cache
- * @param {string} pageUrl - Current page URL
- * @returns {Object} Initialized cache object
+ * Extract meta fields from bid won arguments
+ * @param {Object} meta - Meta object
+ * @returns {Object} Extracted meta fields
  */
-function initializeWonBidsCache(pageUrl) {
+function extractMetaFields(meta) {
   return {
-    pageUrl,
-    timestamp: Date.now(),
-    wonBids: []
+    advertiserDomains: meta.advertiserDomains || [],
+    primaryCatId: meta.primaryCatId || '',
+    secondaryCatIds: meta.secondaryCatIds || [],
+    advertiserName: meta.advertiserName || '',
+    advertiserId: meta.advertiserId || '',
+    brandName: meta.brandName || '',
+    brandId: meta.brandId || ''
   };
+}
+
+/**
+ * Mark auctions as sent
+ * @param {Array} auctionIds - Auction IDs to mark
+ */
+function markAuctionsAsSent(auctionIds) {
+  auctionIds.forEach(auctionId => {
+    if (cache.auctions[auctionId]) {
+      cache.auctions[auctionId].sendStatus |= WON_SENT;
+    }
+  });
 }
 
 // IM Analytics Adapter implementation
@@ -98,39 +134,61 @@ const imAnalyticsAdapter = Object.assign(
       switch (eventType) {
         case EVENTS.AUCTION_INIT:
           logMessage('IM Analytics: AUCTION_INIT', args);
-          cache.auctions[args.auctionId] = {
-            bids: {},
-            pv: {
-              cid: args.cid,
-              name: args.name,
-              action_id: args.action_id,
-              adUnits: args.adUnits,
-            }
-          };
-          this.handleReqBidsData(args);
+          this.handleAuctionInit(args);
           break;
 
         case EVENTS.BID_WON:
           logMessage('IM Analytics: BID_WON', args);
           this.handleWonBidsData(args);
           break;
+
+        case EVENTS.AUCTION_END:
+          logMessage('IM Analytics: AUCTION_END', args);
+          this.scheduleWonBidsSend();
+          break;
       }
     },
 
     /**
-     * Handle request bids data with debounce
-     * @param {Object} auctionArgs - Auction arguments
+     * Handle auction init event
+     * @param {Object} args - Auction arguments
      */
-    handleReqBidsData(auctionArgs) {
-      const pageUrl = window.location.href;
-      if (!cache.ReqBidsData || cache.ReqBidsData.pageUrl !== pageUrl) {
-        cache.ReqBidsData = initializeReqBidsCache(pageUrl);
-      }
-      cache.ReqBidsData.auctions.push(this.transformReqBidsData(auctionArgs));
-      cache.ReqBidsTimer = clearTimer(cache.ReqBidsTimer);
-      cache.ReqBidsTimer = setTimeout(() => {
-        this.sendReqBidsData();
-      }, DEBOUNCE_DELAY);
+    handleAuctionInit(args) {
+      const consentData = getConsentData(args.bidderRequests);
+
+      cache.auctions[args.auctionId] = {
+        consentData: consentData,
+        sendStatus: 0
+      };
+
+      this.handleAucInitData(args, consentData);
+    },
+
+    /**
+     * Schedule won bids send after timeout
+     */
+    scheduleWonBidsSend() {
+      cache.wonBidsTimer = clearTimer(cache.wonBidsTimer);
+      cache.wonBidsTimer = setTimeout(() => {
+        this.sendWonBidsData();
+      }, BID_WON_TIMEOUT);
+    },
+
+    /**
+     * Handle auction init data - send immediately for PV tracking
+     * @param {Object} auctionArgs - Auction arguments
+     * @param {Object} consentData - Consent data object
+     */
+    handleAucInitData(auctionArgs, consentData) {
+      const payload = {
+        pageUrl: window.location.href,
+        referrer: document.referrer || '',
+        consentData,
+        timestamp: Date.now(),
+        auction: this.transformAucInitData(auctionArgs)
+      };
+
+      sendToApi(buildApiUrlWithOptions(this.options, 'pv'), payload);
     },
 
     /**
@@ -138,84 +196,99 @@ const imAnalyticsAdapter = Object.assign(
      * @param {Object} auctionArgs - Auction arguments
      * @returns {Object} Transformed auction data
      */
-    transformReqBidsData(auctionArgs) {
+    transformAucInitData(auctionArgs) {
       return {
         auctionId: auctionArgs.auctionId,
         pv: auctionArgs.pv,
         timestamp: auctionArgs.timestamp,
-        timeout: auctionArgs.timeout,
         adUnitCodes: auctionArgs.adUnitCodes || [],
-        adUnits: (auctionArgs.adUnits || []).map(adUnit => {
-          return {
-            code: adUnit.code,
-            mediaTypes: adUnit.mediaTypes,
-            sizes: adUnit.sizes,
-            bids: (adUnit.bids || []).map(bid => ({
-              bidder: bid.bidder,
-              params: bid.paramsr
-            }))
-          }
-        })
+        adUnitCount: (auctionArgs.adUnits || []).length
       };
     },
 
     /**
-     * Send accumulated request bids data to API
-     */
-    sendReqBidsData() {
-      if (!cache.ReqBidsData) return;
-      const cid = getCid(this.options);
-      sendToApi(buildApiUrl(cid, 'request'), cache.ReqBidsData);
-      cache.ReqBidsData = null;
-      cache.ReqBidsTimer = null;
-    },
-
-    /**
-     * Handle won bids data with debounce
+     * Handle won bids data - batch first, then individual
      * @param {Object} bidWonArgs - Bid won arguments
      */
     handleWonBidsData(bidWonArgs) {
-      const pageUrl = window.location.href;
-      if (!cache.wonBidsData || cache.wonBidsData.pageUrl !== pageUrl) {
-        cache.wonBidsData = initializeWonBidsCache(pageUrl);
+      const auctionId = bidWonArgs.auctionId;
+      const auction = cache.auctions[auctionId];
+
+      if (!auction) return;
+
+      // If initial batch has been sent, send immediately
+      if (auction.sendStatus & WON_SENT) {
+        this.sendIndividualWonBid(auctionId, bidWonArgs, auction.consentData);
+      } else {
+        this.cacheWonBid(auctionId, bidWonArgs);
       }
-      cache.wonBidsData.wonBids.push(this.transformWonBidsData(bidWonArgs));
-      cache.wonBidsTimer = clearTimer(cache.wonBidsTimer);
-      cache.wonBidsTimer = setTimeout(() => {
-        this.sendWonBidsData();
-      }, DEBOUNCE_DELAY);
     },
 
     /**
-     * Transform won bids data for bid won event
+     * Send individual won bid immediately
+     * @param {string} auctionId - Auction ID
      * @param {Object} bidWonArgs - Bid won arguments
-     * @returns {Object} Transformed won bids data
+     * @param {Object} consentData - Consent data
+     */
+    sendIndividualWonBid(auctionId, bidWonArgs, consentData) {
+      const wonBid = this.transformWonBidsData(bidWonArgs);
+
+      sendToApi(buildApiUrlWithOptions(this.options, 'won'), {
+        consentData: consentData || getConsentData(null),
+        wonBids: {
+          [auctionId]: [wonBid]
+        }
+      });
+    },
+
+    /**
+     * Cache won bid for batch send
+     * @param {string} auctionId - Auction ID
+     * @param {Object} bidWonArgs - Bid won arguments
+     */
+    cacheWonBid(auctionId, bidWonArgs) {
+      if (!cache.wonBidsData[auctionId]) {
+        cache.wonBidsData[auctionId] = [];
+      }
+      cache.wonBidsData[auctionId].push(this.transformWonBidsData(bidWonArgs));
+    },
+
+    /**
+     * Transform bid won data for payload
+     * @param {Object} bidWonArgs - Bid won arguments
+     * @returns {Object} Transformed bid won data
      */
     transformWonBidsData(bidWonArgs) {
+      const meta = bidWonArgs.meta || {};
+
       return {
         auctionId: bidWonArgs.auctionId,
-        adId: bidWonArgs.adId,
-        adUnitCode: bidWonArgs.adUnitCode,
-        bidder: bidWonArgs.bidder || bidWonArgs.bidderCode,
-        cpm: bidWonArgs.cpm,
-        currency: bidWonArgs.currency,
-        creativeId: bidWonArgs.creativeId,
-        dealId: bidWonArgs.dealId,
-        mediaType: bidWonArgs.mediaType,
-        size: bidWonArgs.size,
-        timeToRespond: bidWonArgs.timeToRespond,
-        meta: bidWonArgs.meta || {}
+        timestamp: Date.now(),
+        bidder: bidWonArgs.bidder,
+        bidderCode: bidWonArgs.bidderCode,
+        ...extractMetaFields(meta)
       };
     },
 
     /**
-     * Send accumulated won bids data to API
+     * Send accumulated won bids data to API - batch send after 800ms
      */
     sendWonBidsData() {
-      if (!cache.wonBidsData) return;
-      const cid = getCid(this.options);
-      sendToApi(buildApiUrl(cid, 'won'), cache.wonBidsData);
-      cache.wonBidsData = null;
+      if (Object.keys(cache.wonBidsData).length === 0) return;
+
+      const auctionIds = Object.keys(cache.wonBidsData);
+      const firstAuction = cache.auctions[auctionIds[0]];
+      const consentData = firstAuction ? firstAuction.consentData : getConsentData(null);
+
+      sendToApi(buildApiUrlWithOptions(this.options, 'won'), {
+        consentData,
+        wonBids: cache.wonBidsData
+      });
+
+      markAuctionsAsSent(auctionIds);
+
+      // Clear cache
+      cache.wonBidsData = {};
       cache.wonBidsTimer = null;
     }
   }
